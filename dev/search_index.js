f207ab7db6e04b5ac66eab5758638ef2a8dc1969
Documenter.jl <documenter@juliadocs.github.io>
2022-12-12 23:55:28 +0000

    build based on 1aead00

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReturnAssumptions","category":"page"},{"location":"#ReturnAssumptions","page":"Home","title":"ReturnAssumptions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReturnAssumptions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReturnAssumptions]","category":"page"}]
+}
