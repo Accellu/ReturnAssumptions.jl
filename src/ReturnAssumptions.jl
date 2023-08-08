dfd1c91e3f6329eceafd550655f8f61b609a7ac2
j-h-stevens <115172566+j-h-stevens@users.noreply.github.com>
2023-08-08 19:17:49 -0400

    Add Euro and MSCI_Emerging

--- src/ReturnAssumptions.jl
+++ src/ReturnAssumptions.jl
@@ -2,4 +2,17 @@ module ReturnAssumptions
 
 using JSON3
 
+ports = copy(JSON3.read(read("assets/Model_Portfolios.json")))
+
+allocs = ports[:Allocations]
+allocs[:MSCI_Emerging] = vcat([0, 0, 0, 1], zeros(13))
+allocs[:MSCI_EAFE]
+allocs[:MSCI_EAFE] = vcat([0, 0, 1], zeros(14))
+allocs[:Euro_Stoxx] = vcat([0, 0, 1], zeros(14))
+
+open("assets/Model_Portfolios.json", "w") do io
+    JSON3.pretty(io, allocs)
+end
+
+
 end
